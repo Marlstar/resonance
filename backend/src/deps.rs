b6fc9bb4720a6d2d7b8eaf07e4cfdf810b8af330
Marley Reeves <marley@marlstar.com>
2025-05-09 14:13:32 +1000

    feat: ffmpeg autoinstall capability

---
+++ backend/src/deps.rs
@@ -0,0 +1,68 @@
+use crate::util::flush_stdout;
+
+struct ProgressBar {
+    printed: usize,
+}
+impl ProgressBar {
+    pub fn new() -> Self {
+        Self { printed: 0 }
+    }
+
+    pub fn start(&self) {
+        print!(" > ");
+        flush_stdout();
+    }
+
+    pub fn update(&mut self, percentage: usize) {
+        let a = percentage / 10;
+        if a > self.printed {
+            for _ in 0..(a - self.printed) { print!("#") }
+            flush_stdout();
+            self.printed = a;
+        }
+    }
+}
+
+pub async fn install_ffmpeg() {
+    use essi_ffmpeg::FFmpeg;
+    use essi_ffmpeg::FFmpegDownloadProgress;
+
+    FFmpeg::override_downloaded_ffmpeg_path(crate::dirs().deps()).unwrap();
+
+    if let Some((_handle, mut progress)) = FFmpeg::auto_download().await.unwrap() {
+        println!("[deps] ffmpeg not found, installing");
+
+        let (mut started, mut extracting) = (false, false);
+        let mut progressbar = ProgressBar::new();
+
+        'recv: while let Some(state) = progress.recv().await {
+            match state {
+                FFmpegDownloadProgress::Starting => {},
+                FFmpegDownloadProgress::Downloading(status) => {
+                    if !started {
+                        started = true;
+                        print!("[install-ffmpeg] downloading");
+                        flush_stdout();
+                        progressbar.start();
+                    }
+                    if let Some(s) = status { progressbar.update(s); }
+                },
+                FFmpegDownloadProgress::Extracting => {
+                    if !extracting {
+                        println!(" => DONE");
+                        extracting = true;
+                        print!("[install-ffmpeg] extracting");
+                        flush_stdout();
+                    }
+                },
+                FFmpegDownloadProgress::Finished => {
+                    println!(" => DONE");
+                    println!("[install-ffmpeg] install complete");
+                    break 'recv;
+                },
+            }
+        }
+    } else {
+        println!("[deps] ffmpeg installation found");
+    }
+}
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,4 @@
 fn main() {
     println!("You ran the backend silly");
+    tokio::runtime::Runtime::new().unwrap().block_on(resonance_backend::deps::install_ffmpeg());
 }
