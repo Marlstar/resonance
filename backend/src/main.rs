0bf7ac1b98053b69d26cf7fb252fb3d873cc896b
Marley Reeves <marley@marlstar.com>
2025-05-20 16:27:20 +1000

    refactor(backend): remove `main.rs` to make backend no longer a binary crate
    Fixes dist issues

--- backend/src/main.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    println!("You ran the backend silly");
-    // tokio::runtime::Runtime::new().unwrap().block_on(resonance_backend::deps::install_ffmpeg());
-    tokio::runtime::Runtime::new().unwrap().block_on(resonance_backend::deps::install_ytdlp());
-}
