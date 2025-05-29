f29614c9e0db47f024b82d97c42601ae1e14deb5
Marley Reeves <marley@marlstar.com>
2025-05-29 20:38:18 +1000

    feat: setup database if it doesn't exist
    - Makes the application fully self-sufficient (I believe) for the first time. No other cli tools or setup needed.

--- backend/src/db/mod.rs
+++ backend/src/db/mod.rs
@@ -1,3 +1,4 @@
+pub mod setup;
 pub mod schema;
 pub mod util;
 pub use util::connect;
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,7 +1,8 @@
 use resonance_frontend::Resonance;
 
 fn main() -> iced::Result {
-    backend::deps::install_deps();
+    backend::deps::install_deps(); // Install ffmpeg and yt-dlp
+    backend::db::setup::run_migrations(); // Setup database
 
     iced::application("Resonance", Resonance::update, Resonance::view)
         .theme(Resonance::theme)
