141602e3008ad9af3359155ee1f97d5aea4ee5fa
Marley Reeves <marley@marlstar.com>
2025-06-17 09:37:02 +1000

    feat(util): get youtube id from url

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,5 +1,12 @@
 use std::path::Path;
+use regex::Regex;
 
 pub fn display_path(p: &Path) -> String {
     return format!("{}", p.display());
+}
+
+pub fn ytid_from_yt_url(url: &str) -> Option<String> {
+    let re = Regex::new(r".*\.youtube\.com/watch\?v=(?<id>(?:\w|-)+).*").unwrap();
+    let captures = re.captures(url)?;
+    return Some(captures["id"].to_string());
 }
