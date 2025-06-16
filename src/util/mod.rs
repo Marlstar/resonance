f129cafed18cf6ccc222784dcc53753f82bcb96a
Marley Reeves <marley@marlstar.com>
2025-06-17 09:46:28 +1000

    test(util): youtube id regex test

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -14,3 +14,13 @@ pub fn ytid_from_yt_url(url: &str) -> Option<String> {
 pub fn yt_url_from_ytid(id: &str) -> String {
     return format!("https://music.youtube.com/watch?v={id}");
 }
+
+mod tests {
+    #[test]
+    pub fn ytid_from_yt_url() {
+        assert_eq!(
+            super::ytid_from_yt_url("https://music.youtube.com/watch?v=ZqVDbGlDzzo&si=pBYLnOQnrJ5-o7xV"),
+            Some("ZqVDbGlDzzo".to_string())
+        );
+    }
+}
