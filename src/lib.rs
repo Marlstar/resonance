0ad575a06a88eac92743cdb6935d242696e8a493
Marley Reeves <marley@marlstar.com>
2025-06-16 20:33:10 +1000

    feat(db): model for `Song`s

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,6 @@ pub mod common;
 pub use common::dirs;
 
 pub mod db;
+
+pub mod models;
+pub use models::song::Song;
---
+++ src/models/song.rs
@@ -0,0 +1,48 @@
+use diesel::{insert_into, prelude::*};
+
+#[derive(Debug, Clone, PartialEq)]
+#[derive(Queryable, Selectable)]
+#[diesel(table_name = crate::db::schema::songs)]
+#[diesel(check_for_backend(diesel::sqlite::Sqlite))]
+pub struct Song {
+    pub id: i32,
+    pub ytid: Option<String>,
+    pub name: String,
+    pub artist: Option<i32>,
+    pub album: Option<i32>,
+    pub duration: i32,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = crate::db::schema::songs)]
+pub struct NewSong<'a> {
+    pub ytid: Option<&'a str>,
+    pub name: &'a str,
+    pub artist: Option<i32>,
+    pub album: Option<i32>,
+    pub duration: i32,
+}
+
+pub fn create(
+    conn: &mut SqliteConnection,
+    ytid: Option<&str>,
+    name: &str,
+    artist: Option<i32>,
+    album: Option<i32>,
+    duration: i32
+) -> Result<Song, diesel::result::Error> {
+    use crate::db::schema::songs;
+
+    let new_song = NewSong {
+        ytid,
+        name,
+        artist,
+        album,
+        duration
+    };
+
+    insert_into(songs::table)
+        .values(&new_song)
+        .returning(Song::as_returning())
+        .get_result(conn)
+}
