2e90cffe8803ba2fa18ddd94484b119280353087
Marley Reeves <marley@marlstar.com>
2025-06-16 19:03:26 +1000

    fix(dirs): use `.local/share` instead of `.local/state`, use `resonancev2` subdirectory

--- src/common/dirs.rs
+++ src/common/dirs.rs
@@ -2,14 +2,15 @@ use std::sync::LazyLock;
 use std::path::PathBuf;
 use directories::BaseDirs;
 pub static BASE: LazyLock<BaseDirs> = LazyLock::new(|| BaseDirs::new().unwrap());
+pub static SHARE: LazyLock<PathBuf> = LazyLock::new(|| BASE.data_dir().join("resonancev2"));
 
-macro_rules! path {
+macro_rules! share {
     ($name:ident, $ext:expr) => {
-        pub static $name: LazyLock<PathBuf> = LazyLock::new(|| (&*BASE).state_dir().unwrap().join($ext).to_path_buf());
+        pub static $name: LazyLock<PathBuf> = LazyLock::new(|| (&*SHARE).join($ext).to_path_buf());
     };
     ($name:ident) => {
-        pub static $name: LazyLock<PathBuf> = LazyLock::new(|| (&*BASE).state_dir().unwrap().to_path_buf());
+        pub static $name: LazyLock<PathBuf> = LazyLock::new(|| (&*SHARE).to_path_buf());
     };
 }
 
-path!(DATABASE, "resonance.db");
+share!(DATABASE, "resonance.db");
