a6c20f0a5a1b0490bc1737afbcc30f71e72f3ddb
Marley Reeves <marley@marlstar.com>
2025-06-17 15:34:39 +1000

    feat(dirs): intermediate cover dir

--- src/common/dirs/cover.rs
+++ src/common/dirs/cover.rs
@@ -7,6 +7,9 @@ pub fn yt(ytid: &str) -> PathBuf {
 pub fn yt_filename(ytid: &str) -> String {
     format!("y-{ytid}.{COVER_FORMAT}")
 }
+pub fn yt_intermediate(ytid: &str) -> PathBuf {
+    return crate::dirs::SONGS.join(yt_filename_intermediate(ytid));
+}
 pub fn yt_filename_intermediate(ytid: &str) -> String {
     format!("y-{ytid}.webp")
 }
