d0cfe2807ec5a5db45305f84b9d6cafc35c675b4
Marley Reeves <marley@marlstar.com>
2025-07-23 14:32:52 +1000

    refactor(models): move individual `ModelExt` traits into one `ModelsExt` trait

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -7,13 +7,5 @@ pub use album::Album;
 pub mod artist;
 pub use artist::Artist;
 
-mod get_model;
-pub use get_model::*;
-
-pub mod prelude {
-    pub use super::Song;
-    pub use super::Artist;
-    pub use super::Album;
-
-    pub use super::get_model::*;
-}
+mod modelsext;
+pub use modelsext::ModelsExt;
--- src/models/modelsext.rs
+++ src/models/modelsext.rs
@@ -1,20 +1,18 @@
 use super::{Song, Artist, Album};
 
-pub trait GetSongExt { fn get_song(self) -> Option<Song>; }
-pub trait GetArtistExt { fn get_artist(self) -> Option<Artist>; }
-pub trait GetAlbumExt { fn get_album(self) -> Option<Album>; }
+pub trait ModelsExt {
+    fn get_song(self) -> Option<Song>;
+    fn get_artist(self) -> Option<Artist>;
+    fn get_album(self) -> Option<Album>;
+}
 
-impl GetSongExt for i32 {
+impl ModelsExt for i32 {
     fn get_song(self) -> Option<Song> {
         Song::get(self).ok()?
     }
-}
-impl GetArtistExt for i32 {
     fn get_artist(self) -> Option<Artist> {
         Artist::get(self).ok()?
     }
-}
-impl GetAlbumExt for i32 {
     fn get_album(self) -> Option<Album> {
         Album::get(self).ok()?
     }
