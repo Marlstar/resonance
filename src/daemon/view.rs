c522ebd4548099c66b365affb6676ed33a18d77d
Marley Reeves <marley@marlstar.com>
2025-08-01 14:03:57 +1000

    fix(windows): conform to new window module structure in `Daemon::view`

--- src/daemon/view.rs
+++ src/daemon/view.rs
@@ -1,11 +1,10 @@
 use crate::iced::types::*;
-use crate::windows::main::MainWindow;
-use crate::windows::settings::SettingsWindow;
+use crate::windows;
 
 impl super::Daemon {
     pub fn view(&self, window: iced::window::Id) -> Element {
-        if Some(window) == self.windows.main { MainWindow::view(self) }
-        else if Some(window) == self.windows.settings { SettingsWindow::view(self) }
+        if Some(window) == self.windows.main { windows::main::view(self) }
+        else if Some(window) == self.windows.settings { windows::settings::view(self) }
         else { iced::widget::text("Unknown window - make sure to render it in Daemon::view").into() }
     }
 }
