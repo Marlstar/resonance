c4e640222704a6d23f421f571cebdff0c4d99cb8
Marley Reeves <marley@marlstar.com>
2025-08-01 11:19:18 +1000

    fix(tray): discolouration of tray icon due to format mismatch (RGBA->ARGB)

--- src/tray/mod.rs
+++ src/tray/mod.rs
@@ -12,9 +12,23 @@ pub fn create() -> TrayItem {
     let (tx, rx) = unbounded::<TrayEvent>();
     let _ = EVENT_RX.get_or_init(move || rx);
 
+    let icon = crate::assets::icon_rgba_256().to_vec();
+    let mut bytes = Vec::<u8>::with_capacity(icon.len());
+    let mut iter = icon.into_iter();
+    while let Some(r) = iter.next() {
+        let g = iter.next().unwrap();
+        let b = iter.next().unwrap();
+        let a = iter.next().unwrap();
+        // convert to ARGB
+        bytes.push(a);
+        bytes.push(r);
+        bytes.push(g);
+        bytes.push(b);
+    };
+
     let icon = IconSource::Data {
         width: 256, height: 256,
-        data: crate::assets::icon_rgba_256().to_vec(),
+        data: bytes,
     };
     let mut tray = TrayItem::new("Resonance", icon).expect("failed to initialise tray icon");
     
