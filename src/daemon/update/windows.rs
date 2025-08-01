e7b4613707506980986ef812248454c52a0fa92a
Marley Reeves <marley@marlstar.com>
2025-08-01 11:59:13 +1000

    refactor(windows): macro window open task functions

--- src/daemon/update/windows.rs
+++ src/daemon/update/windows.rs
@@ -3,19 +3,22 @@ use crate::daemon::Message;
 use crate::iced::types::Task;
 use crate::windows;
 
+macro_rules! open_window {
+    ($window:ident, $s:expr) => {{
+        let (id, task) = windows::$window::open();
+        $s.windows.$window = Some(id);
+        notify_open(&stringify!($window), id);
+        task.map(|_| Message::None)
+    }}
+}
+
 impl super::super::Daemon {
     pub(super) fn open_main_window(&mut self) -> Task {
-        let (id, task) = windows::main::open();
-        self.windows.main = Some(id);
-        notify_open("main", id);
-        task.map(|_| Message::None)
+        open_window!(main, self)
     }
 
     pub(super) fn open_settings_window(&mut self) -> Task {
-        let (id, task) = windows::settings::open();
-        self.windows.settings = Some(id);
-        notify_open("settings", id);
-        task.map(|_| Message::None)
+        open_window!(settings, self)
     }
 
     pub(super) fn handle_window_closed(&mut self, id: Id) -> Task {
